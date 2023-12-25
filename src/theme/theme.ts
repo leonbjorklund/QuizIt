d9abf3d767be32f159a5af0fef74017ef160056f
ParhamInBinart <parham.berenjian@gmail.com>
2023-12-25 10:25:24 +0100

    implement chakraUI and set base theme colors

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  primary: '#0A192F',
+  secondary: '#132F59',
+  accent: '#147849',
+  button: '#FAF089',
+  selected: '#325386',
+};
+
+export const theme = extendTheme({ colors });
