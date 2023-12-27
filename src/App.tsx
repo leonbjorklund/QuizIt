73ad168b7e5ff90fd7e50b99c475526cceef2b66
ParhamInBinart <parham.berenjian@gmail.com>
2023-12-27 09:59:11 +0100

    render scene base

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import { MainWrapperStyle, PageWrapperStyle } from './GlobalStyles';
-import { Header, HomeScene } from './components';
+import { EndScene, Header, HomeScene, LoadingScene, OptionsScene, PlayScene } from './components';
 import { MainContainer } from './theme/MainContainer';
 
+enum Scene {
+  HOME = 'home',
+  OPTIONS = 'options',
+  LOADING = 'loading',
+  PLAY = 'play',
+  END = 'end',
+}
+
 export default function App() {
+  const [scene, setScene] = useState<Scene>(Scene.HOME);
+
+  const renderScene = (clickedScene: Scene) => {
+    switch (clickedScene) {
+      case Scene.HOME:
+        return <HomeScene />;
+      case Scene.OPTIONS:
+        return <OptionsScene />;
+      case Scene.LOADING:
+        return <LoadingScene />;
+      case Scene.PLAY:
+        return <PlayScene />;
+      case Scene.END:
+        return <EndScene />;
+    }
+  };
+
   return (
     <Flex sx={PageWrapperStyle}>
       <Header />
       <Flex as="main" sx={MainWrapperStyle}>
-        <MainContainer>
-          <HomeScene />
-        </MainContainer>
+        <MainContainer>{renderScene(scene)}</MainContainer>
       </Flex>
     </Flex>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export * from './end';
 export * from './Header';
 export * from './HomeScene';
 export * from './LoadingScene';
 export * from './OptionsScene';
 export * from './PlayScene';
+export * from './EndScene';
