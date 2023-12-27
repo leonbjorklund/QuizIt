6c505e9018b65dda49e907900661c1c71b5071ed
ParhamInBinart <parham.berenjian@gmail.com>
2023-12-27 15:40:11 +0100

    set theme + strings for home components

--- src/components/HomeScene/HomeScene.tsx
+++ src/components/HomeScene/HomeScene.tsx
@@ -4,6 +4,7 @@ import { Button, Image, Input, InputGroup, InputRightElement, Text, Textarea, us
 import { useState } from 'react';
 import { Scene } from '../../App';
 import { LogoBlack, OGLogo } from '../../assets/images';
+import { homeStrings } from '../../assets/strings';
 import { HomeSceneLogoStyle } from './styles';
 
 interface IHome {
@@ -17,6 +18,7 @@ enum InputType {
 
 export const HomeScene = ({ setScene }: IHome) => {
   const { colorMode } = useColorMode();
+  const { subtitle, urlPlaceholder, textPlaceholder, continueBtn } = homeStrings;
 
   const [inputType, setInputType] = useState<InputType>(InputType.URL);
   const [inputValue, setInputValue] = useState('');
@@ -36,63 +38,42 @@ export const HomeScene = ({ setScene }: IHome) => {
   return (
     <>
       <Image src={colorMode === 'dark' ? OGLogo : LogoBlack} alt="QuizItLogo" sx={HomeSceneLogoStyle} />
-      <Text textAlign="center">Enter a URL below to generate a quiz about anything.</Text>
+      <Text textAlign="center">{subtitle}</Text>
       <InputGroup maxWidth="600px">
         {inputType === InputType.URL ? (
           <Input
-            mt=".25rem"
-            placeholder="Enter URL here..."
+            placeholder={urlPlaceholder}
             name="searchbar"
-            size={{ base: 'sm', sm: 'md' }}
-            border="none"
-            py="1.4rem"
-            pr="4rem"
-            bg={colorMode === 'dark' ? '#2D3748' : '#E2E8EF'}
-            _placeholder={{ color: colorMode === 'dark' ? 'rgba(255, 255, 255, 0.6)' : 'rgba(0, 0, 0, 0.6)' }}
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
           />
         ) : (
           <Textarea
-            resize="none"
-            height="15rem"
-            mt=".25rem"
-            placeholder="Enter your text here..."
+            placeholder={textPlaceholder}
             name="searchbar"
-            pr="4rem"
-            size={{ base: 'sm', sm: 'md' }}
-            border="none"
-            bg={colorMode === 'dark' ? '#2D3748' : '#E2E8EF'}
-            _placeholder={{ color: colorMode === 'dark' ? 'rgba(255, 255, 255, 0.6)' : 'rgba(0, 0, 0, 0.6)' }}
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
           />
         )}
 
-        <InputRightElement mt=".4rem" mr=".2rem">
+        <InputRightElement>
           {inputValue !== '' ? (
-            <Button variant="rounded" h=".5rem" onClick={() => setInputValue('')}>
-              <CloseIcon color="rgba(255, 255, 255, 0.6)" />
+            <Button variant="searchbarBtn" onClick={() => setInputValue('')}>
+              <CloseIcon />
             </Button>
           ) : inputType === InputType.URL ? (
-            <Button onClick={handleSwitchInputType}>
-              <EditIcon color="rgba(255, 255, 255, 0.6)" />
+            <Button variant="searchbarBtn" onClick={handleSwitchInputType}>
+              <EditIcon />
             </Button>
           ) : (
-            <Button onClick={handleSwitchInputType}>
-              <LinkIcon color="rgba(255, 255, 255, 0.6)" />
+            <Button variant="searchbarBtn" onClick={handleSwitchInputType}>
+              <LinkIcon />
             </Button>
           )}
         </InputRightElement>
       </InputGroup>
-      <Button
-        p="15px 30px"
-        variant="proceed"
-        mt="1rem"
-        fontSize={{ base: 'xs', sm: 'sm', md: 'md' }}
-        onClick={() => handleNextScene(Scene.PLAY)}
-      >
-        Continue
+      <Button p="15px 30px" variant="proceed" mt="1rem" onClick={() => handleNextScene(Scene.PLAY)}>
+        {continueBtn}
       </Button>
     </>
   );
