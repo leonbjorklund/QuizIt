a0b4004fd7bdf6f5e9588efb2311cd2f0120c50a
ParhamInBinart <parham.berenjian@gmail.com>
2024-01-05 19:39:25 +0100

    quizData implememted in playScene

--- src/components/PlayScene/PlayScene.tsx
+++ src/components/PlayScene/PlayScene.tsx
@@ -1,77 +1,91 @@
-import { Text } from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { Button, Flex, HStack, Heading, Icon, IconButton, RadioGroup, Text } from '@chakra-ui/react';
 
+import { playStrings } from '../../assets';
+import { Radio, SceneCard, SceneContainer } from '../../chakra';
 import { useAppContext } from '../../context/AppContext';
+import { NavigateQuestion, Scene } from '../../utils/types';
+import {
+  AnswerFlexStyle,
+  BottomButtomStackStyle,
+  PreviousQuestionButtonStyle,
+  ProceedButtonStyle,
+  QuestionTextStyle,
+  TopTextStackStyle,
+} from './styles';
+import useQuiz from './useQuiz';
 
 export const PlayScene = () => {
-  const { quizData } = useAppContext();
+  const { quizData, setScene } = useAppContext();
+  const { btns } = playStrings;
 
-  if (!quizData) {
-    return <div>Loading...</div>;
-  }
-  console.log(quizData);
-
-  // const {
-  //   index,
-  //   currentQuestion,
-  //   value,
-  //   showAnswer,
-  //   // score, Används senare
-  //   userAnswers,
-  //   setQuizState,
-  //   navigateQuestion,
-  //   checkAnswer,
-  //   renderIcon,
-  // } = useQuiz();
+  const {
+    index,
+    currentQuestion,
+    value,
+    showAnswer,
+    score /* ANVÄNDS SENARE */,
+    userAnswers,
+    setQuizState,
+    navigateQuestion,
+    checkAnswer,
+    renderIcon,
+  } = useQuiz();
 
   return (
-    <Text>PlayScene</Text>
-    // <SceneContainer variant="playScene">
-    //   <HStack sx={TopTextStackStyle}>
-    //     <Text>
-    //       {/* {index + 1} / {quizData.quiz.questions.length} */}
-    //       {/* <Text>{JSON.stringify(quizData, null, 2)}</Text> */}
-    //     </Text>
-    //   </HStack>
-    //   <SceneCard variant="playCard">
-    //     <Heading sx={QuestionTextStyle}>{currentQuestion.question}</Heading>
-    //     <RadioGroup
-    //       isDisabled={showAnswer}
-    //       w="100%"
-    //       value={value}
-    //       onChange={(e) => setQuizState((prev) => ({ ...prev, value: e }))}
-    //     >
-    //       <Flex sx={AnswerFlexStyle}>
-    //         {currentQuestion.options?.map((option, i) => (
-    //           <Radio
-    //             variant="playQuiz"
-    //             key={i}
-    //             isPlayQuizScene
-    //             value={option}
-    //             showAnswer={showAnswer}
-    //             isCorrectOption={showAnswer && option === currentQuestion.answer}
-    //             isChecked={value === option}
-    //             isUserPreviousChoice={userAnswers[index] === option}
-    //           >
-    //             <Icon boxSize="1.5rem" as={renderIcon(option)} />
-    //             {option}
-    //           </Radio>
-    //         ))}
-    //       </Flex>
-    //     </RadioGroup>
-    //   </SceneCard>
-    //   <HStack sx={BottomButtomStackStyle}>
-    //     {index > 0 && (
-    //       <IconButton
-    //         sx={PreviousQuestionButtonStyle}
-    //         icon={<ChevronLeftIcon boxSize="2rem" />}
-    //         aria-label="previous-question"
-    //         onClick={() => navigateQuestion('previous')}
-    //       />
-    //     )}
-    //     <Button sx={ProceedButtonStyle} variant="proceed" onClick={checkAnswer} isDisabled={!value && !showAnswer}>
-    //       {showAnswer ? 'Next Question' : 'Reveal Answer'}
-    //     </Button>
-    //   </HStack>
-    // </SceneContainer>
+    <SceneContainer variant="playScene">
+      <HStack sx={TopTextStackStyle}>
+        <Text>
+          {index + 1} / {quizData.quiz.questions.length}
+          {/* <Text>{JSON.stringify(quizData, null, 2)}</Text> */}
+        </Text>
+      </HStack>
+      <SceneCard variant="playCard">
+        <Heading sx={QuestionTextStyle}>{currentQuestion.question}</Heading>
+        <RadioGroup
+          isDisabled={showAnswer}
+          w="100%"
+          value={value}
+          onChange={(e) => setQuizState((prev) => ({ ...prev, value: e }))}
+        >
+          <Flex sx={AnswerFlexStyle}>
+            {currentQuestion.options?.map((option, i) => (
+              <Radio
+                variant="playQuiz"
+                key={i}
+                isPlayQuizScene
+                value={option}
+                showAnswer={showAnswer}
+                isCorrectOption={showAnswer && option === currentQuestion.answer}
+                isChecked={value === option}
+                isUserPreviousChoice={userAnswers[index] === option}
+              >
+                <Icon boxSize="1.5rem" as={renderIcon(option)} />
+                {option}
+              </Radio>
+            ))}
+          </Flex>
+        </RadioGroup>
+      </SceneCard>
+      <HStack sx={BottomButtomStackStyle}>
+        {index > 0 && (
+          <IconButton
+            sx={PreviousQuestionButtonStyle}
+            icon={<ChevronLeftIcon boxSize="2rem" />}
+            aria-label="previous-question"
+            onClick={() => navigateQuestion(NavigateQuestion.PREV)}
+          />
+        )}
+        {index === quizData.quiz.questions.length -1 && showAnswer ? (
+          <Button variant="proceed" onClick={() => setScene(Scene.END)}>
+            {btns.result}
+          </Button>
+        ) : (
+          <Button sx={ProceedButtonStyle} variant="proceed" onClick={checkAnswer} isDisabled={!value && !showAnswer}>
+            {showAnswer ? btns.next : btns.reveal}
+          </Button>
+        )}
+      </HStack>
+    </SceneContainer>
   );
 };
--- src/components/PlayScene/useQuiz.tsx
+++ src/components/PlayScene/useQuiz.tsx
@@ -1,105 +1,106 @@
-// import { useCallback, useEffect, useState } from 'react';
-// import { IoMdCloseCircleOutline } from 'react-icons/io';
-// import { MdOutlineCheckCircle, MdRadioButtonChecked, MdRadioButtonUnchecked } from 'react-icons/md';
+import { useCallback, useEffect, useState } from 'react';
+import { IoMdCloseCircleOutline } from 'react-icons/io';
+import { MdOutlineCheckCircle, MdRadioButtonChecked, MdRadioButtonUnchecked } from 'react-icons/md';
 
-// import { QuizData } from '../../utils/types';
-// import quizData from './quizData.json';
+import { useAppContext } from '../../context/AppContext';
+import { NavigateQuestion, QuizData } from '../../utils/types';
 
-// // export type QuizData = {
-// //   quiz: {
-// //     quiz: string;
-// //     questions: {
-// //       question: string;
-// //       options: string[];
-// //       answer: string;
-// //     }[];
-// //   };
-// // };
-
-// type QuizState = {
-//   index: number;
-//   currentQuestion: QuizData['quiz']['questions'][number];
-//   value: string;
-//   showAnswer: boolean;
-//   userAnswers: { [key: number]: string };
-// };
-
-// const useQuizTest = () => {
-//   console.log('quizData', quizData);
-//   const [quizState, setQuizState] = useState<QuizState>({
-//     index: 0,
-//     currentQuestion: quizData?.quiz?.questions ? quizData.quiz.questions[0] : null,
-//     value: '',
-//     showAnswer: false,
-//     userAnswers: {},
-//   });
-//   const [score, setScore] = useState(0);
-//   const [answeredQuestions, setAnsweredQuestions] = useState<number[]>([]);
-
-//   useEffect(() => {
-//     setQuizState((prevState) => ({
-//       ...prevState,
-//       currentQuestion: quizData.quiz.questions[prevState.index],
-//       showAnswer: answeredQuestions.includes(prevState.index),
-//     }));
-//   }, [quizState.index, answeredQuestions]);
-
-//   const navigateQuestion = useCallback((direction: 'next' | 'previous') => {
-//     setQuizState((prevState) => {
-//       let newIndex = direction === 'next' ? prevState.index + 1 : prevState.index - 1;
-//       newIndex = Math.max(0, Math.min(newIndex, quizData.quiz.questions.length - 1)); // Corrected path
-//       return {
-//         ...prevState,
-//         index: newIndex,
-//         value: '',
-//       };
-//     });
-//   }, []);
-
-//   const checkAnswer = useCallback(() => {
-//     const isAnswerShown = quizState.showAnswer;
-//     if (!isAnswerShown) {
-//       const isCorrect = quizState.value === quizState.currentQuestion.answer;
-//       setQuizState((prevState) => ({
-//         ...prevState,
-//         showAnswer: true,
-//         userAnswers: { ...prevState.userAnswers, [prevState.index]: quizState.value },
-//       }));
-//       setAnsweredQuestions((prev) => [...prev, quizState.index]);
-
-//       if (isCorrect) {
-//         setScore((prevScore) => prevScore + 1);
-//       }
-//     } else {
-//       navigateQuestion('next');
-//     }
-//   }, [quizState, navigateQuestion]);
-
-//   const renderIcon = useCallback(
-//     (option: string) => {
-//       const { showAnswer, currentQuestion, userAnswers, index, value } = quizState;
-//       if (showAnswer) {
-//         if (option === currentQuestion.answer) {
-//           return MdOutlineCheckCircle;
-//         }
-//         if (option === userAnswers[index]) {
-//           return IoMdCloseCircleOutline;
-//         }
-//       }
-//       return option === value ? MdRadioButtonChecked : MdRadioButtonUnchecked;
-//     },
-//     [quizState],
-//   );
-
-//   return {
-//     ...quizState,
-//     score,
-//     answeredQuestions,
-//     setQuizState,
-//     navigateQuestion,
-//     checkAnswer,
-//     renderIcon,
+// export type QuizData = {
+//   quiz: {
+//     quiz: string;
+//     questions: {
+//       question: string;
+//       options: string[];
+//       answer: string;
+//     }[];
 //   };
 // };
 
-// export default useQuizTest;
+type QuizState = {
+  index: number;
+  currentQuestion: QuizData['quiz']['questions'][number];
+  value: string;
+  showAnswer: boolean;
+  userAnswers: { [key: number]: string };
+};
+
+const useQuizTest = () => {
+  const { quizData } = useAppContext();
+  console.log('quizData', quizData);
+  const [quizState, setQuizState] = useState<QuizState>({
+    index: 0,
+    currentQuestion: quizData?.quiz?.questions ? quizData.quiz.questions[0] : null,
+    value: '',
+    showAnswer: false,
+    userAnswers: {},
+  });
+  const [score, setScore] = useState(0);
+  const [answeredQuestions, setAnsweredQuestions] = useState<number[]>([]);
+
+  useEffect(() => {
+    setQuizState((prevState) => ({
+      ...prevState,
+      currentQuestion: quizData.quiz.questions[prevState.index],
+      showAnswer: answeredQuestions.includes(prevState.index),
+    }));
+  }, [quizState.index, answeredQuestions]);
+
+  const navigateQuestion = useCallback((direction: NavigateQuestion.NEXT | NavigateQuestion.PREV) => {
+    setQuizState((prevState) => {
+      let newIndex = direction === NavigateQuestion.NEXT ? prevState.index + 1 : prevState.index - 1;
+      newIndex = Math.max(0, Math.min(newIndex, quizData.quiz.questions.length - 1)); // Corrected path
+      return {
+        ...prevState,
+        index: newIndex,
+        value: '',
+      };
+    });
+  }, []);
+
+  const checkAnswer = useCallback(() => {
+    const isAnswerShown = quizState.showAnswer;
+    if (!isAnswerShown) {
+      const isCorrect = quizState.value === quizState.currentQuestion.answer;
+      setQuizState((prevState) => ({
+        ...prevState,
+        showAnswer: true,
+        userAnswers: { ...prevState.userAnswers, [prevState.index]: quizState.value },
+      }));
+      setAnsweredQuestions((prev) => [...prev, quizState.index]);
+
+      if (isCorrect) {
+        setScore((prevScore) => prevScore + 1);
+      }
+    } else {
+      navigateQuestion(NavigateQuestion.NEXT);
+    }
+  }, [quizState, navigateQuestion]);
+
+  const renderIcon = useCallback(
+    (option: string) => {
+      const { showAnswer, currentQuestion, userAnswers, index, value } = quizState;
+      if (showAnswer) {
+        if (option === currentQuestion.answer) {
+          return MdOutlineCheckCircle;
+        }
+        if (option === userAnswers[index]) {
+          return IoMdCloseCircleOutline;
+        }
+      }
+      return option === value ? MdRadioButtonChecked : MdRadioButtonUnchecked;
+    },
+    [quizState],
+  );
+
+  return {
+    ...quizState,
+    score,
+    answeredQuestions,
+    setQuizState,
+    navigateQuestion,
+    checkAnswer,
+    renderIcon,
+  };
+};
+
+export default useQuizTest;
